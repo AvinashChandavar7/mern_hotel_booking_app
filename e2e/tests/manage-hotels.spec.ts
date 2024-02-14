78cb9c5891fcb63b94144487f0dae3434c82751d
avinashChandavar7 <avinashChnadavar7@gmail.com>
2024-02-14 20:47:23 +0530

    feat : add test for my-hotels

--- e2e/tests/manage-hotels.spec.ts
+++ e2e/tests/manage-hotels.spec.ts
@@ -54,3 +54,21 @@ test("should allow user to add a hotel",
     await expect(page.getByText("Hotel Saved!")).toBeVisible({ timeout: 20000 });
   }
 )
+
+test("should display hotels",
+  async ({ page }) => {
+    await page.goto(`${UI_URL}my-hotels`);
+
+    await expect(page.getByText("Dublin Getaways")).toBeVisible();
+    await expect(page.getByText("Dublin, Ireland")).toBeVisible();
+    await expect(page.getByText("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")).toBeVisible();
+    await expect(page.getByText("₹4998 & All Inclusive")).toBeVisible();
+    await expect(page.getByText("2 adults, 3 children")).toBeVisible();
+    await expect(page.getByText("2 Star rating")).toBeVisible();
+    await expect(page.getByText("Spa, Free WiFi, Family Rooms, Airport Shuttle")).toBeVisible();
+
+
+    await expect(page.getByRole("link", { name: "View Details" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Add Hotel" })).toBeVisible();
+  }
+)
