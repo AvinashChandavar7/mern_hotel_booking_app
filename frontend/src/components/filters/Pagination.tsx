e10cee2bb2656792b8b02cbf37b2ec0145f41c51
avinashChandavar7 <avinashChnadavar7@gmail.com>
2024-02-18 19:46:45 +0530

    fix : the Pagination issue click the button

--- frontend/src/components/filters/Pagination.tsx
+++ frontend/src/components/filters/Pagination.tsx
@@ -22,10 +22,9 @@ function Pagination({ page, pages, onPageChange }: PaginationProps) {
     <div className="flex items-center justify-center">
       <ul className="flex items-center justify-center w-full gap-2 rounded-md border-slate-300">
         {pageNumbers.map((number, index) => (
-          <li key={index} className={`px-4 py-2 cursor-pointer font-bold rounded-md border border-blue-500 ${page === number ? "bg-blue-500 text-white" : ""}`}>
-            <button onClick={() => memoizedOnPageChange(number)}>
-              {number}
-            </button>
+          <li key={index} onClick={() => memoizedOnPageChange(number)}
+            className={`px-4 py-2 cursor-pointer font-bold rounded-md border border-blue-500 ${page === number ? "bg-blue-500 text-white" : ""}`}>
+            <button>{number}</button>
           </li>
         ))}
       </ul>
