99ade6f5232b81639248019c4928ca16a0d41217
avinashChandavar7 <avinashChnadavar7@gmail.com>
2024-02-19 16:44:39 +0530

    test : add search-hotels

---
+++ e2e/tests/search-hotels.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+
+const UI_URL = "http://localhost:5173/";
+
+test.beforeEach(
+  async ({ page }) => {
+    await page.goto(UI_URL);
+
+    // get the Login in Button
+    await page.getByRole("link", { name: "Sign In" }).click();
+
+    await expect(page.getByRole("heading", { name: "Login" })).toBeVisible();
+
+    await page.locator("[name=email]").fill("aaaa@gmail.com");
+    await page.locator("[name=password]").fill("123456");
+
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await expect(page.getByText("Login Success!")).toBeVisible();
+  }
+);
+
+test("Should show hotel search results", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByPlaceholder("Where are you going").fill("Dublin");
+
+  await page.getByRole("button", { name: "Search" }).click();
+
+  await expect(page.getByText("Hotels Found in Dublin")).toBeVisible();
+  await expect(page.getByText("Dublin Getaways").first()).toBeVisible();
+
+})
